8136e20e049e39e34ea047c1455b248ea7e728f6
md-saju-ahmed <hellomdsajuahmed@gmail.com>
2026-06-09 18:25:53 +0600

    update app layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import Footer from "@/components/shared/Footer";
 import { ToastContainer } from "react-toastify";
+import { CartProvider } from "@/context/CartContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,18 +17,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      data-theme="light"
-      className="h-full antialiased"
-    >
+    <html lang="en" data-theme="light" className="h-full antialiased">
       <body
         className={`${geistSans.className} min-h-full flex flex-col scroll-smooth animate__animated animate__fadeIn`}
       >
-        <Navbar />
-        {children}
-        <Footer />
-        <ToastContainer position="top-right" autoClose={1000} />
+        <CartProvider>
+          <Navbar />
+          {children}
+          <Footer />
+          <ToastContainer position="top-right" autoClose={1000} />
+        </CartProvider>
       </body>
     </html>
   );
